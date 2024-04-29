c0ba5cf900479295bce4edce52f2245b5927ea7a
Dipesh Dwivedi <112308318+Dipesh412@users.noreply.github.com>
2024-04-29 12:15:41 +0530

    Time: 11 ms (37.66%) | Memory: 10.1 MB (5.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -7,10 +7,9 @@ public:
         unordered_map<char,int>mp;
         for(int i=0; i<s.size(); i++){
             mp[s[i]]++;
-        }
-        for(int i=0; i<t.size(); i++){
             mp[t[i]]--;
         }
+        
         for(int i=0; i<mp.size(); i++){
             if(mp[i]>0){
                 return false;
