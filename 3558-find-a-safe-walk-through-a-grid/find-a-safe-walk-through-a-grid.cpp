dcccd198717a30b616954ab04438d7a59d00e67c
Dipesh Dwivedi <112308318+Dipesh412@users.noreply.github.com>
2024-09-15 12:01:46 +0530

    Time: 51 ms (40.00%) | Memory: 34.7 MB (70.00%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& g, int health) {
+        int m = g.size(), n = g[0].size(), cnt[51][51] = {};
+        priority_queue<array<int, 3>> pq;
+        pq.push({health - g[0][0], 0, 0});
+        while (!pq.empty()) {
+            auto [h, i, j] = pq.top(); pq.pop();
+            if (i == m - 1 && j == n - 1)
+                return true;
+            for (auto [dx, dy] : vector<pair<int, int>>{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}) {
+                int x = i + dx, y = j + dy;
+                if (min(x, y) >= 0 && x < m && y < n && cnt[x][y] < h - g[x][y]) {
+                    cnt[x][y] = h - g[x][y];
+                    pq.push({h - g[x][y], x, y});
+                }
+            }
+        }
+        return false;
+    }
+};
