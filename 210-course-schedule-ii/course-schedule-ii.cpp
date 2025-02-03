3dd3e79f3adc74cff8b021ec9d99c37b2ecc0605
Dipesh Dwivedi <112308318+Dipesh412@users.noreply.github.com>
2025-02-03 23:34:19 +0530

    Time: 7 ms (36.84%) | Memory: 18.7 MB (35.12%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
-        vector<int>adj[n];
+    bool dfs(vector<int>&vis, int node, vector<int> adj[], stack<int>&st, vector<int>&patVis){
+        vis[node] = 1;
+        patVis[node] =1;
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if(dfs(vis,it,adj,st,patVis)== true){
+                    return true;
+                }
+            }else if(patVis[it] ){
+                return true;
+            }
+            
+        }
+        st.push(node); 
+        patVis[node] =0;
+        return false;
+    }
+    vector<int> findOrder(int v, vector<vector<int>>& prerequisites) {
+        vector<int>vis(v,0);
+        vector<int>patVis(v,0);
+        vector<int>adj[v];
         for ( auto it:prerequisites){
             adj[it[1]].push_back(it[0]);
         }
-        vector<int>indegree(n,0);
-        for(int i=0; i<n; i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
-            }
-        }
-        queue<int>q;
-        for(int i=0; i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
         vector<int>ans;
-        while(!q.empty()){
-            int node = q.front();
-            ans.push_back(node);
-            q.pop();
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
+        stack<int>st;
+        int l = 0;
+        for(int i=0; i<v; i++){
+            if(!vis[i]){
+                if(dfs(vis,i,adj,st,patVis)== true){
+                    return {};
                 }
             }
         }
-        if(ans.size()==n) return ans;
-        return {};
+        
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop(); 
+        } 
+        return ans;
     }
 };
