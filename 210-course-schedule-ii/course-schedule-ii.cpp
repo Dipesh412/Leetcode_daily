865ba3ef96fe2cb757db6b741e5df3b3aefb5d8b
Dipesh Dwivedi <112308318+Dipesh412@users.noreply.github.com>
2025-02-03 23:44:29 +0530

    Time: 8 ms (26.20%) | Memory: 18.3 MB (61.79%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-    bool dfs(vector<int>&vis, int node, vector<int> adj[], stack<int>&st, vector<int>&patVis){
-        vis[node] = 1;
-        patVis[node] =1;
-        for(auto it : adj[node]){
-            if(!vis[it]){
-                if(dfs(vis,it,adj,st,patVis)== true){
-                    return true;
-                }
-            }else if(patVis[it] ){
-                return true;
-            }
-            
-        }
-        st.push(node); 
-        patVis[node] =0;
-        return false;
-    }
-    vector<int> findOrder(int v, vector<vector<int>>& prerequisites) {
-        vector<int>vis(v,0);
-        vector<int>patVis(v,0);
-        vector<int>adj[v];
-        for ( auto it:prerequisites){
-            adj[it[1]].push_back(it[0]);
-        }
+    vector<int> findOrder(int n, vector<vector<int>>& ra) {
+        vector<int>inde(n,0);
+        vector<int>adj[n];
         vector<int>ans;
-        stack<int>st;
-        int l = 0;
-        for(int i=0; i<v; i++){
-            if(!vis[i]){
-                if(dfs(vis,i,adj,st,patVis)== true){
-                    return {};
-                }
+        for(auto it: ra){
+            adj[it[1]].push_back(it[0]); 
+        }
+
+        for(int i=0; i<n; i++){
+            for(auto it : adj[i]){
+                inde[it]++;
             }
         }
-        
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop(); 
+
+        queue<int>q;
+        for(int i=0; i<n; i++){
+            if(inde[i]==0){
+                q.push(i); 
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node); 
+            for(auto it : adj[node]){
+                inde[it]--;
+                if(inde[it]==0){
+                    q.push(it); 
+                }
+            }
         } 
-        return ans;
+        if(ans.size()==n) return ans;
+        return {};
     }
 };
