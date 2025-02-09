96a892dfc68a7c5a34d3a0916cc2a05469a027d2
Dipesh Dwivedi <112308318+Dipesh412@users.noreply.github.com>
2025-02-09 17:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (60.70%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int f(int i,int j,vector<vector<int>>&dp,int n,vector<vector<int>>&triangle){
-        if(i==n-1) return triangle[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = triangle[i][j] +f(i+1,j,dp,n,triangle);
-        int dia = triangle[i][j]+f(i+1,j+1,dp,n,triangle);
-        return dp[i][j] = min(down,dia);
+    int f(int i, int j, int n, vector<vector<int>>& tri, vector<vector<int>>&dp){
+        if(i==n-1){
+            return tri[i][j];
+
+        }
+        if(dp[i][j] !=-1 ){
+            return dp[i][j];
+        }
+        int down = tri[i][j] + f(i+1,j,n,tri,dp);
+        int di = tri[i][j] + f(i+1,j+1,n,tri,dp);
+        return dp[i][j] = min (down,di);
     }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        // int m = triangle[0].size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(0,0,dp,n,triangle);
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n = tri.size();
+        //int m = tri[0].size();
+        vector<vector<int>>dp(n, vector<int>(n,-1));
+        return f(0,0,n,tri,dp);
     }
 };
