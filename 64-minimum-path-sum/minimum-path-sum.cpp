aef938c7d98956284b71737884e252813e25347b
Dipesh Dwivedi <112308318+Dipesh412@users.noreply.github.com>
2025-02-09 17:03:08 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (14.30%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int f(int i,int j,vector<vector<int>>&dp,vector<vector<int>>&grid){
+    int f(int i, int j , vector<vector<int>>& grid,  vector<vector<int>>&dp){
         if(i==0 && j==0) return grid[0][0];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up = INT_MAX,left=INT_MAX;
-        if(i-1>=0){
-            up = grid[i][j]+f(i-1,j,dp,grid);
-        }
-        if(j-1>=0){
-            left = grid[i][j]+f(i,j-1,dp,grid);
-        }
-        return dp[i][j] = min(left,up);
+        if(i < 0 || j<0) return 1e8;
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = grid[i][j] + f(i-1,j,grid,dp);
+        int left = grid[i][j] + f(i,j-1,grid,dp);
+           
+        return  dp[i][j] = min (up,left); 
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,dp,grid);
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,grid,dp);
     }
 };
